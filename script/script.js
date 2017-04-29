72de58bd15c7b5627b5664922565ac7b12058629
“JASON” <jrcorey84@gmail.com>
2017-04-29 16:36:54 -0500

    added design page elements, and images with lightbox

--- script/script.js
+++ script/script.js
@@ -1,5 +1,9 @@
 console.log('working')
 
+$('.logoOne, .logoTwo, .logoThree, .logoFour').css('cursor', 'pointer');
+$('.keyArt1, .keyArt2, .keyArt3, .keyArt4, .keyArt5, .keyArt6').css('cursor', 'pointer');
+
+
 //Header items slide
 $(document).ready(function () {
     $('.linkBar').slideDown(500).css('display','flex');
@@ -22,4 +26,63 @@ $('.audibleLogo').click(function(){
     	items: ['images/audible/audible1.jpg','images/audible/audible2.jpg','images/audible/audible3.jpg']
 	});
 });
+
+$('.cafLogo').click(function(){
+	$.SimpleLightbox.open({
+    	items: ['images/caf/boat-tours.jpg','images/caf/bike-tours.jpg','images/caf/happy-hour-tours.jpg']
+	});
+});
+
+// Lighbox for design
+
+$('.keyArt1').click(function(){
+	$.SimpleLightbox.open({
+    	items: ['images/posters/all-is-lost.jpg']
+	});
+});
+
+$('.keyArt2').click(function(){
+	$.SimpleLightbox.open({
+    	items: ['images/posters/thin-red-line.jpg']
+	});
+});
+
+$('.keyArt3').click(function(){
+	$.SimpleLightbox.open({
+    	items: ['images/posters/mad-men.jpg']
+	});
+});
+
+$('.keyArt4').click(function(){
+	$.SimpleLightbox.open({
+    	items: ['images/posters/layer-cake.jpg']
+	});
+});
+
+$('.keyArt5').click(function(){
+	$.SimpleLightbox.open({
+    	items: ['images/posters/american-psycho.jpg']
+	});
+});
+
+$('.keyArt6').click(function(){
+	$.SimpleLightbox.open({
+    	items: ['images/posters/friday-the-13th.jpg']
+	});
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
