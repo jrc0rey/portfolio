422c150a78ffbd4de5027c90ff2d2a5fe349cd56
“JASON” <jrcorey84@gmail.com>
2017-04-28 19:05:16 -0500

    lightbox,art direction content,jquery

--- script/script.js
+++ script/script.js
@@ -1,1 +1,25 @@
 console.log('working')
+
+//Header items slide
+$(document).ready(function () {
+    $('.linkBar').slideDown(500).css('display','flex');
+});
+
+$('.intro').hide();
+
+
+//Intro slide
+$(document).ready(function () {
+	setTimeout(function(){
+		$('.intro').show('slide', {direction: 'left'}, 1000);
+	}, 500)
+	
+});
+
+//LightBox for audible.com
+$('.audibleLogo').click(function(){
+	$.SimpleLightbox.open({
+    	items: ['images/audible/audible1.jpg','images/audible/audible2.jpg','images/audible/audible3.jpg']
+	});
+});
+
