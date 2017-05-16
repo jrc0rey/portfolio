df6091227ef1e5a0b28bcb4935b5c009c14e9ab6
“JASON” <jrcorey84@gmail.com>
2017-05-16 13:51:55 -0500

    added web and design elements

--- script/script.js
+++ script/script.js
@@ -6,6 +6,10 @@ $('.keyArt1, .keyArt2, .keyArt3, .keyArt4, .keyArt5, .keyArt6, .keyArt7, .keyArt
 
 $('.logo1, .logo2, .logo3, .transistor').css('cursor', 'pointer');
 
+$('.vecOne, .vecTwo, .vecThree, .vecFour, .vecFive, .vecSix, .vecSeven, .vecEight')
+.css('cursor', 'pointer');
+
+
 
 //Header items slide
 
@@ -21,21 +25,45 @@ $('.intro').hide();
 $(document).ready(function () {
 	setTimeout(function(){
 		$('.intro').show('slide', {direction: 'left'}, 1000);
-	}, 500)	
+	},500);	
 });
 
-//LightBox for audible.com
+//LightBox for art direction
 
 $('.audibleLogo').click(function(){
 	$.SimpleLightbox.open({
-    	items: ['images/audible/audible1.jpg','images/audible/audible2.jpg','images/audible/audible3.jpg',
-    	'https://player.vimeo.com/video/95600745']
+    	items: ['images/audible/audible1.jpg',
+    			'images/audible/audible2.jpg',
+    			'images/audible/audible3.jpg',
+    			'https://player.vimeo.com/video/95600745'],
+    	captions:['*Featured in Archive Magazine']
 	});
 });
 
 $('.cafLogo').click(function(){
 	$.SimpleLightbox.open({
-    	items: ['images/caf/boat-tours.jpg','images/caf/bike-tours.jpg','images/caf/happy-hour-tours.jpg']
+    	items: ['images/caf/boat-tours.jpg',
+    			'images/caf/bike-tours.jpg',
+    			'images/caf/happy-hour-tours.jpg']
+	});
+});
+
+$('.virginLogo').click(function(){
+	$.SimpleLightbox.open({
+    	items: ['images/virgin-mobile/virgin-mobile1.jpg',
+    			'images/virgin-mobile/virgin-mobile2.jpg']
+	});
+});
+
+$('.hwdLogo').click(function(){
+	$.SimpleLightbox.open({
+    	items: ['images/hollywood-casino/hwd-1.jpg',
+    			'images/hollywood-casino/hwd-2.jpg', 
+    			'images/hollywood-casino/hwd-3.jpg', 
+    			'images/hollywood-casino/hwd-4.jpg', 
+    			'images/hollywood-casino/hwd-5.jpg', 
+    			'images/hollywood-casino/hwd-6.jpg', 
+    			'images/hollywood-casino/hwd-7.jpg' ]
 	});
 });
 
@@ -120,6 +148,85 @@ $('.transistor').click(function(){
     	items: ['images/logos/transistor-poster.jpg']
 	});
 });
+
+$('.vecOne').click(function(){
+	$.SimpleLightbox.open({
+    	items: ['images/illustrations/vec-1.jpg']
+	});
+});
+
+$('.vecTwo').click(function(){
+	$.SimpleLightbox.open({
+    	items: ['images/illustrations/vec-2.jpg']
+	});
+});
+
+$('.vecThree').click(function(){
+	$.SimpleLightbox.open({
+    	items: ['images/illustrations/vec-3.jpg']
+	});
+});
+
+$('.vecFour').click(function(){
+	$.SimpleLightbox.open({
+    	items: ['images/illustrations/vec-4.jpg']
+	});
+});
+
+$('.vecFive').click(function(){
+	$.SimpleLightbox.open({
+    	items: ['images/illustrations/vec-5.jpg']
+	});
+});
+
+$('.vecSix').click(function(){
+	$.SimpleLightbox.open({
+    	items: ['images/illustrations/vec-6.jpg']
+	});
+});
+
+$('.vecSeven').click(function(){
+	$.SimpleLightbox.open({
+    	items: ['images/illustrations/vec-7.jpg']
+	});
+});
+
+$('.vecEight').click(function(){
+	$.SimpleLightbox.open({
+    	items: ['images/illustrations/vec-8.jpg']
+	});
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
